f1c127898e9f200bd2df1fd4a63ed4c6f5fafa9a
Admin <jmcox2009@gmail.com>
2019-01-24 17:11:09 -0800

    Project initialized.

---
+++ PlumeWeather/PlumeWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlumeWeather
+//
+//  Created by Admin on 1/24/19.
+//  Copyright © 2019 JosephCox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
